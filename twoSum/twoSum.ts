d36d33dc5fd6a8c7c8d4274ba3f560364c36171f
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-09-01 16:36:09 -0300

    feat: answer to the twoSum problem

--- twoSum/twoSum.ts
+++ twoSum/twoSum.ts
@@ -1,15 +1,16 @@
 function twoSum(nums: number[], target: number): number[] {
-  const result: number[] = [];
-  let sum = 0;
-
-  for (let i = 0; i < nums.length; i++) {
-    
+  if (nums.length === 0) {
+    return [];
   }
 
-  return result;
-}
+  for (let i = 0; i <= nums.length; i++) {
+    for (let j = i + 1; j <= nums.length; j++) {
+      const result = nums[i] + nums[j];
+      if (result === target) {
+        return [i, j];
+      }
+    }
+  }
 
-// console.log(twoSum([2, 7, 11, 15], 9));
-// console.log(twoSum([3, 2, 4], 6));
-// console.log(twoSum([3, 3], 6));
-console.log(twoSum([3, 3, 5, 1, 11, 20, 30, 32, 50], 6));
+  return [];
+}
